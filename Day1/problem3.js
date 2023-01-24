892f859a5faf91bed9e0859a6302a18c6420f354
eddy <eddy1759@gmail.com>
2023-01-24 02:27:45 +0100

    day 1 done

--- Day1/problem3.js
+++ Day1/problem3.js
@@ -6,7 +6,7 @@
  var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++){
         for (var j = i+ 1; j < nums.length; j++){
-            if (nums[i] + nums[j] === target){
+            if (gitnums[i] + nums[j] === target){
                 return [i, j];
             }
         }
